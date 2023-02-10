940340b1f63b6d15cae2ba4b3f7a952310f3e909
Anshul Hajare <anshulhajare2024@u.northwestern.edu>
2023-02-10 17:28:49 -0600

    fnn starter code

--- hw#1/starter.py
+++ hw#1/starter.py
@@ -57,7 +57,7 @@ def read_encode(file_name,vocab,words,corpus,threshold):
             if temp[t][1] >= threshold:
                 vocab.append(t)
                 wID = wID + 1
-                words[t] = [wID,temp[t][1]]
+                words[t] = [wID, temp[t][1]]
             
                     
     with open(file_name,'rt') as f:
@@ -74,7 +74,7 @@ def read_encode(file_name,vocab,words,corpus,threshold):
     return [vocab,words,corpus]
 
 class FFNN(nn.Module):
-    def __init__(self, vocab, words,d_model, d_hidden, dropout):
+    def __init__(self, vocab, words, d_model, d_hidden, dropout):
         super().__init__() 
     
         self.vocab = vocab
@@ -83,16 +83,39 @@ class FFNN(nn.Module):
         self.d_model = d_model
         self.d_hidden = d_hidden
         self.dropout = dropout
-        self.embeds = nn.Embedding(vocab_size,d_model)
+        self.embeds = nn.Embedding(self.vocab_size, d_model)
+        self.linear1 = nn.Linear(50, 128)
+        self.nonLinear1 = nn.ReLU()
+        self.linear2 = nn.Linear(128, 256)
+        self.nonLinear2 = nn.ReLU()
+        self.linear3 = nn.Linear(256, 2)
+        self.logits = nn.Softmax()
+        # self.net = nn.Sequential(
+        #     nn.Linear(9, 128), 
+        #     nn.ReLU(),
+        #     nn.Linear(128, 256),
+        #     nn.ReLU(),
+        #     nn.Linear(256, 2), 
+        #     nn.Softmax()
+        # )
 #          {perform other initializations needed for the FFNN}
 
     def forward(self, src):
-        embeds = self.dropout(self.embeds(src))
+        print(src)
+        # embeds = self.dropout(self.embeds(src))
+        # src = src.type(torch.LongTensor)
+        # print(len(src[0:50]))
+        output = nn.ReLU(self.linear1(src[0:50].long()))
+        output = nn.ReLU(self.linear2(output))
+        output = nn.ReLU(self.linear3(output))
+        return nn.Softmax(output)
+
 #          {add code to implement the FFNN}
-        return x
+        # return self.net(embeds)
                 
     def init_weights(self):
 #          {perform initializations}
+        pass
         
         
 class LSTM(nn.Module):
@@ -105,20 +128,23 @@ class LSTM(nn.Module):
         self.n_layers = n_layers
         self.d_hidden = d_hidden
         self.d_model = d_model
-        self.embeds = nn.Embedding(vocab_size,d_model)
+        self.embeds = nn.Embedding(self.vocab_size, d_model)
 #          {perform other initializations needed for the LSTM}
         
     def forward(self,src,h):
         embeds = self.dropout(self.embeds(src))
-#          {add code to implement the LSTM}        
+#          {add code to implement the LSTM}     
+        pass
         return [preds,h]
     
     def init_weights(self):
 #          {perform initializations}
+        pass
         
     
     def detach_hidden(self, hidden):
 #          {needed for training...}
+        pass
         return [hidden, cell]  
     
 def main():
@@ -138,32 +164,61 @@ def main():
     parser.add_argument('-model', type=str,default='LSTM')
     parser.add_argument('-savename', type=str,default='lstm')
     parser.add_argument('-loadname', type=str)
-    parser.add_argument('-trainname', type=str,default='wiki.train.txt')
-    parser.add_argument('-validname', type=str,default='wiki.valid.txt')
-    parser.add_argument('-testname', type=str,default='wiki.test.txt')
-    
-    params = parser.parse_args()    
+    parser.add_argument('-trainname', type=str, default='wiki.train.txt')
+    parser.add_argument('-validname', type=str, default='wiki.valid.txt')
+    parser.add_argument('-testname', type=str, default='wiki.test.txt')
+    parser.add_argument("-f", required=False)
+    # print(parser)
+    # params = vars(parser.parse_args())
+    params = {
+        'd_model': 100,
+        'd_hidden': 100,
+        'n_layers': 2,
+        'batch_size': 20,
+        'seq_len': 30,
+        'printevery': 5000,
+        'window': 3,
+        'epochs': 20,
+        'lr': 0.0001,
+        'dropout': 0.35,
+        'clip': 2.0,
+        'model': 'FFNN',
+        'savename': 'lstm',
+        'loadname': None,
+        'trainname': 'fake.train.txt',
+        'validname': 'fake.valid.txt',
+        'testname': 'fake.test.txt'
+    }   
+
     torch.manual_seed(0)
     
-    [vocab,words,train] = read_encode(params.trainname,[],{},[],3)
-    print('vocab: %d train: %d' % (len(vocab),len(train)))
-    [vocab,words,test] = read_encode(params.testname,vocab,words,[],-1)
+    [vocab,words,train] = read_encode(params['trainname'],[],{},[],3)
+    print('vocab: %d train: %d' % (len(vocab), len(train)))
+    [vocab,words,test] = read_encode(params['testname'], vocab,words,[],-1)
     print('vocab: %d test: %d' % (len(vocab),len(test)))
-    params.vocab_size = len(vocab)
+    params['vocab_size'] = len(vocab)
     
-    if params.model == 'FFNN':
+    epochs = 1
+    if params['model'] == 'FFNN':
+        model = FFNN(vocab, words, 50, None, 0.1)
+        for i in range(epochs):
+            model.train()
+            
+            output = model(torch.LongTensor(train))
+            
 #          {add code to instantiate the model, train for K epochs and save model to disk}
         
-    if params.model == 'LSTM':
-#          {add code to instantiate the model, train for K epochs and save model to disk}
+#     if params.model == 'LSTM':
+# #          {add code to instantiate the model, train for K epochs and save model to disk}
 
-    if params.model == 'FFNN_CLASSIFY':
-#          {add code to instantiate the model, recall model parameters and perform/learn classification}
+#     if params.model == 'FFNN_CLASSIFY':
+# #          {add code to instantiate the model, recall model parameters and perform/learn classification}
 
-    if params.model == 'LSTM_CLASSIFY':
-#          {add code to instantiate the model, recall model parameters and perform/learn classification}
+#     if params.model == 'LSTM_CLASSIFY':
+# #          {add code to instantiate the model, recall model parameters and perform/learn classification}
         
-    print(params)
+#     print(params)
+
     
 
 if __name__ == "__main__":
